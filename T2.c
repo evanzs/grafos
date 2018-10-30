4b03623884131640fc4d8a22e0fbaf3490d51642
evanzs <evandrofidk@hotmail.com>
2018-10-29 21:45:56 -0300

    menus e coments

--- T2.c
+++ T2.c
@@ -1,18 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+// Trabalho de ed2 sobre grafos e buscas
 
 
-
-
-int main ()
+menus ()
 {
-	
-	
-	int op =0;
-	
-	
-	
 	printf ("\t\t\t\t Grafos\n\n\n");
 	
 	
@@ -21,24 +14,61 @@ int main ()
 	printf ("\t1.\t Busca em profundidade\n");
 	printf ("\t2.\t Busca em Largura\n");
 	printf ("\t3.\t Dijkstra\n");
-	printf ("\t4.\t Ordenacao Topologica\n\n\n\n");
+	printf ("\t4.\t Ordenacao Topologica\n");
+	printf ("\t0. \t Sair\n\n\n\n");
+}
+
+
+
+int main ()
+{
 	
 	
+	int op =0;
+	char opp;
+	Menus();
 	
-	do
+	
+	//controlando entrada de dados validas
+	do	
 	{
 		printf ("Escolha a opcao desejada: ");
 		scanf ("%d",&op);
-		if (( op > 4) || (op < 1))
-			printf ("Nao existe essa opcao!!\n\n");
+		if (( op > 4) || (op < 0))
+			printf ("Nao existe essa opcao!!\n\n");		
+		
+	}while (( op > 4) || (op< 0));
+	
+	
+	
+	
+	//sair do programa
+	if ( op == 0)
+	{
+		do				
+		{
+		printf ("Tem certeza que deseja sair? S/N");
+		scanf ("%c",&opp);
+	
+		if ((opp == "S" ) || (opp== "s"))
+		{
+			exit();	
+		}
+		if ((opp == "N") || (opp=="n"))
+		{
+			system("cls");
+			menus();
+			opp =1;
+		}
+		
+		}while (opp == 0);
+	
 		
 		
-	}while (( op > 4) || (op< 1));
-	
-	
-	
-	
-	
+		
+		
+	}
+			
 	
 	
 	
